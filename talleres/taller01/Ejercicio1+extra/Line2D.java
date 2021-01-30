02b27dba6a7611ce488d6fb2951a8d5b551c452c
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2021-01-29 19:40:10 -0500

    Update Line2D.java

--- talleres/taller01/Ejercicio1+extra/Line2D.java
+++ talleres/taller01/Ejercicio1+extra/Line2D.java
@@ -11,32 +11,27 @@ public class Line2D {
     public Line2D(Punto one, Punto two) {
         this.one = one;
         this.two = two;
+        this.pendiente = (two.getY() - one.getY()) / (two.getX() - one.getX());
+        this.intercepto = (-1.0 * pendiente * this.one.getX()) + this.one.getY();
+        this.distance = this.one.distance(two);
     }
 
     public double getPendiente() {
-        return pendiente;
+        return this.pendiente;
     }
 
     public double getInterceptoY() {
-        return intercepto;
+        return this.intercepto;
     }
 
     public double getDistance() {
-        return distance = one.distance(two);
-    }
-
-    public void setPendiente() {
-        pendiente = (two.getY() - one.getY()) / (two.getX() - one.getX());
-    }
-
-    public void setInterceptoY() {
-        intercepto = (-1.0 * pendiente * one.getX()) + one.getY();
+        return this.distance;
     }
 
     // y=mx+b
     // y-y1=m(x-x1)
     public ArrayList<Punto> middleCoordinates() {
-        for (int i = 0; i <= two.x; i++) {
+        for (int i = 0; i <= two.getX(); i++) {
             coordenadas.add(new Punto(i, (this.pendiente * i) + intercepto));
         }
         return coordenadas;
