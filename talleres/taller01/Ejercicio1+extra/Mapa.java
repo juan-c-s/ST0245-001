ea89b7e761e0cd3cb78d6265827ff3a8378d66a2
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2021-01-29 19:41:35 -0500

    Update Mapa.java

--- talleres/taller01/Ejercicio1+extra/Mapa.java
+++ talleres/taller01/Ejercicio1+extra/Mapa.java
@@ -1,23 +1,21 @@
+
 import java.util.ArrayList;
 
 public class Mapa {
-    private int distancia;
+    private double distancia;
     private ArrayList<Punto> mapa = new ArrayList<>();
-
-    public Mapa() {
-    }
 
     public void añadirPunto(Punto punto) {
         mapa.add(punto);
     }
 
-    public int encontrarDistancia() {
+    public double encontrarDistancia() {
 
         for (int i = 0; i < this.mapa.size() - 1; i++) {
-            distancia += Math.sqrt(Math.pow(mapa.get(i + 1).getY() - mapa.get(i).getY(), 2)
+            this.distancia += Math.sqrt(Math.pow(mapa.get(i + 1).getY() - mapa.get(i).getY(), 2)
                     + Math.pow(mapa.get(i + 1).getX() - mapa.get(i).getX(), 2));
         }
-        return distancia;
+        return this.distancia;
     }
 
 }
