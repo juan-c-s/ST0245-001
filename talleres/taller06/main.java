bcb0dac9af59c226d577c4e48b962283c79e97b5
juan-c-s <63207344+juan-c-s@users.noreply.github.com>
2021-03-05 17:30:37 -0500

    Add files via upload

---
+++ talleres/taller06/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args) {
+        ArrayList2 arr = new ArrayList2();
+        arr.addElement(5);
+        arr.addElement(4);
+        arr.addElement(2);
+        arr.addElement(5);
+        arr.addElement(4);
+        System.out.println(arr.toString());
+    }
+}
